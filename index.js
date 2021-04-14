22631f89bbdfb47651b5da2ccfcdae31b726985e
Josh Searle <joshryansearle@gmail.com>
2021-04-14 15:54:03 +1000

    Add boilerplate for bots

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const discord = require('discord.js');
-console.log('lskdjflkj');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token } = require('./token.json');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', (message) => {
+  if (!startsWith(';;generate', message.content)) return;
+  const letters = message.content.split(' ')[1].split('');
+  console.log(letters);
+});
+
+const startsWith = (prefix, string) => {
+  return prefix.split('').reduce((acc, cur, i) => {
+    return acc && string[i] === cur;
+  }, true);
+};
+
+client.login(token);
